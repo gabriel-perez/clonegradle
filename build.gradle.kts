7c83e150e27e5c9fbd375f76c209c41f66af8e0c
Gabriel David <gabrieldps@gmail.com>
2019-07-04 16:49:27 -0300

    Fix source paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,3 @@ tasks {
     }
 }
 
-kotlin.sourceSets["main"].kotlin.srcDirs("src")
-kotlin.sourceSets["test"].kotlin.srcDirs("test")
-
-sourceSets["main"].resources.srcDirs("resources")
-sourceSets["test"].resources.srcDirs("testresources")
